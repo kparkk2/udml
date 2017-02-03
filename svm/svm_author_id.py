8e80a2771f1b4a9162a684de7141616a51615ef0
kparkk <eliath2@gmail.com>
2017-02-03 13:47:33 +0900

    naive bayes quiz

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,7 +19,7 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+print( "classification start..." )
 
 
 #########################################################
@@ -27,4 +27,36 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 
+from sklearn.svm import SVC
 
+# linear kernel
+#clf = SVC(kernel='linear')
+
+# rbf kernel
+#clf = SVC(kernel='rbf', C=1.)
+#clf = SVC(kernel='rbf', C=10.)
+#clf = SVC(kernel='rbf', C=100.)
+#clf = SVC(kernel='rbf', C=1000.)
+clf = SVC(kernel='rbf', C=10000.)
+
+# slice inputs by 100
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+clf.fit(features_train, labels_train)
+
+print("fitting complete. prediction start")
+
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+
+accuracy = accuracy_score(labels_test, pred)
+print("accuracy: " + str(accuracy))
+
+num_class_one = 0
+for i in range(len(pred)):
+    if pred[i] == 1:
+        num_class_one += 1
+
+print(num_class_one)
